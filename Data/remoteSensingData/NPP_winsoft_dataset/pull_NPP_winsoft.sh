553a70d256d3331efeb0e0a8917dfa1bf5afc8f0
Patrick Daniel <patcdaniel@gmail.com>
2020-03-24 09:02:51 -0700

    Added Wimsoft pull scripts

---
+++ Data/remoteSensingData/NPP_winsoft_dataset/pull_NPP_winsoft.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in $(seq 1996 2019); do
+  FILENAME=npp_$i.zip;
+  URL=http://www.wimsoft.com/CC4km/$FILENAME
+  echo 'Pulling '$FILENAME' from '$URL;
+  curl -sS $URL > $FILENAME;
+  unzip $FILENAME;
+  rm $FILENAME;
+  sleep 3;
+done
